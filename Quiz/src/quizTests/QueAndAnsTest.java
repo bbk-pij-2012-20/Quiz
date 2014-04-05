abe638ba5d09bfd8f7e77f66e973ec72e8f91cc8
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-05 15:37:56 +0100

    testGetMaxInRange() written first (before testIsNicelyDistributed() as isNicelyDistributed() calls getMaxInRange())

--- Quiz/src/quizTests/QueAndAnsTest.java
+++ Quiz/src/quizTests/QueAndAnsTest.java
@@ -51,12 +51,55 @@ public class QueAndAnsTest {
 
 	@Test
 	public void testComposeQuestionIndices() {
-		fail("Not yet implemented");
+		
+		int randomQuestionIndex = qAObj.composeQuestionIndices(0);
+		boolean aValidQuestionIndex = true;
+		
+		if (randomQuestionIndex > 2 && randomQuestionIndex < 0) {
+			
+			aValidQuestionIndex = false;
+			break;
+			
+		}
+		
+		boolean actualAnswer = aValidQuestionIndex;
+		assertTrue(actualAnswer);
+		
 	}
 
 	@Test
-	public void testComposeFalseAnswer() {
-		fail("Not yet implemented");
+	public void testGetMaxInRange() {
+		
+		qAObj.getQue_AnsList()[0] = 0; // how many colours in flag
+		int actualAnswer = qAObj.getMaxInRange();
+		int expectedAnswer = 5;
+				
+		assertEquals(expectedAnswer, actualAnswer);
+		
 	}
+
+	@Test
+	public void testIsNicelyDistributed() {
+		
+		
+		
+	}
+			
+	@Test
+	public void testComposeFalseAnswer() {
+		
+		int falseAnswer = qAObj.composeFalseAnswer();
+		boolean aValidFalseAnswer = true;
+		
+		if (randomQuestionIndex > 2 && randomQuestionIndex < 0) {
+			
+			aValidQuestionIndex = false;
+			break;
+			
+		}
+		
+		boolean actualAnswer = aValidQuestionIndex;
+		assertTrue(actualAnswer);
+
 
 }

87c02fbc788d6fcad74ee651f01dca59f9a061c0
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-05 11:52:46 +0100

    Javadoc QuizService

--- Quiz/src/quiz/QuizService.java
+++ Quiz/src/quiz/QuizService.java
@@ -8,15 +8,17 @@ public interface QuizService extends Remote {
 
 	/**
 	 * Gives the setUpClient options on design of the quiz.
-	 * @param numberOfQuestions
-	 * @param numberOfAnswersPerQuestions
-	 * @throws RemoteException
+	 * 
+	 * @param numberOfQuestions             number of questions per quiz
+	 * @param numberOfAnswersPerQuestions   number of answers per question for multiple choice (includes 1 correct answer)
+	 * @throws RemoteException              in case anything goes wrong with network connectivity.
 	 */
 	void setUpQuiz(int numberOfQuestions, int numberOfAnswersPerQuestions) throws RemoteException;
 	
 	/**
-	 * Gives the playerClient to play the quiz as most recently set up by setUpClient.
-	 * @throws RemoteException
+	 * Gives the playerClient option to begin a new quiz, with parameters set up by setUpClient.
+	 * 
+	 * @throws RemoteException              in case anything goes wrong with network connectivity.
 	 */
 	void playQuiz() throws RemoteException;
 

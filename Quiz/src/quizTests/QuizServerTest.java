41753dc5ef29808291c7beba523b3287876b87ce
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-05 18:54:21 +0100

    testSetUpQuiz() written.

--- Quiz/src/quizTests/QuizServerTest.java
+++ Quiz/src/quizTests/QuizServerTest.java
@@ -2,33 +2,68 @@ package quizTests;
 
 import static org.junit.Assert.*;
 
+import java.rmi.RemoteException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import quiz.QueAndAns;
+import quiz.QuizServer;
+
 public class QuizServerTest {
 
+	QuizServer serverObj;
+	
 	@Before
 	public void setUp() throws Exception {
+	
+		serverObj = new QuizServer();
+	
 	}
 
 	@After
 	public void tearDown() throws Exception {
+	
+		serverObj = null;
+		
 	}
 
 	@Test
 	public void testQuizServer() {
-		fail("Not yet implemented");
+		//constructor ...todo
 	}
 
 	@Test
-	public void testSetUpQuiz() {
-		fail("Not yet implemented");
+	public void testSetUpQuiz() throws RemoteException {
+		
+		int numOfQ = 7;
+		int numOfAnsPerQ = 5;
+
+		serverObj.setUpQuiz(numOfQ,numOfAnsPerQ);
+		
+		int memberFieldQue = serverObj.totalNoOfQuestions;//visibility needs to be temporarily made public for test. 
+		int memberFieldAns = serverObj.noOfAnswersPerQuestion;//visibility needs to be temporarily made public for test.
+ 		String actualValue = "" + memberFieldQue + memberFieldAns;
+		String expectedValue = "75"; 
+		
+		
+		assertEquals(expectedValue, actualValue);
+		
 	}
 
 	@Test
 	public void testPlayQuiz() {
-		fail("Not yet implemented");
-	}
 
+		//todo
+		//assertEquals(expectedValue, actualValue);
+
+	}
+/*
+	@Test
+	public void testMakeListOfQAndAList() {
+	
+		int actualValue2 = serverObj.listOfQAndALists.length;//visibility needs to be temporarily made public for test.
+		int expectedValue2 = 7;	
 }
+*/

0bc3809dedde23f2ed5ef74868a747aa6c0cc88a
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-05 11:46:10 +0100

    Member fields: int questionListIndex, int countryListIndex, int[] que_AnsList and QuizDatabase database.

--- Quiz/src/quiz/QueAndAnsImpl.java
+++ Quiz/src/quiz/QueAndAnsImpl.java
@@ -2,6 +2,11 @@ package quiz;
 
 public class QueAndAnsImpl implements QueAndAns {
 
+	private int	questionListIndex;
+	private int	countryListIndex;
+	private int[] que_AnsList = new int[6];
+	private QuizDatabase database;
+
 	@Override
 	public void generateQueAndAnsList() {
 		// TODO Auto-generated method stub

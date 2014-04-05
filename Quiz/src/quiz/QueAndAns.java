ae13a5080be496fbaf8868283f709b0637b408df
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-05 15:02:24 +0100

    Deleted composeQuestionIndices(int) and composeFalseAnswer(). (These methods were made private in the implementing class).

--- Quiz/src/quiz/QueAndAns.java
+++ Quiz/src/quiz/QueAndAns.java
@@ -7,27 +7,8 @@ public interface QueAndAns {
 	 * which is the correct answer.
 	 * 
 	 */
-	void generateQueAndAnsList();
-	
-	/**
-	 * Composes a question made of two parts by random selection of each part from two lists stored in QuizDatabase.
-	 * 
-	 * @param listIndex   0 or 1, the position of data in the QueAndAns list. (0 holds index of question type, 1 holds 
-	 *                    the index of country name).  
-	 *                    
-	 */
-	void composeQuestionIndices(int listIndex);
-	
-	
-	/**
-	 * Generates a random numerical answer within pre-defined boundaries and with margins of separation from other 
-	 * answers. 
-	 * 
-	 * @return int   returns an int that is within a valid range and is separated from 
-	 *               the other 3 answers.
-	 */
-	int composeFalseAnswer();
-	
+	void generateQueAndAnsList();	
+		
 	/**
 	 * Getter for question and answer list que_AnsList.
 	 * 

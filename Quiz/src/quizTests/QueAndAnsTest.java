5e22c04dc6fa70f2fd5015a47efc59c99bb6b047
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-05 15:03:33 +0100

    testGenerateQueAndAnsList() written. Simply tests that the array is initialised and contains non-zero values throughout.

--- Quiz/src/quizTests/QueAndAnsTest.java
+++ Quiz/src/quizTests/QueAndAnsTest.java
@@ -6,19 +6,47 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import quiz.QueAndAnsImpl;
+
 public class QueAndAnsTest {
 
+	QueAndAnsImpl qAObj; 
+	
 	@Before
 	public void setUp() throws Exception {
+	
+		qAObj = new QueAndAnsImpl();
+	
 	}
 
 	@After
 	public void tearDown() throws Exception {
+	
+		qAObj = null;
+		
 	}
 
 	@Test
 	public void testGenerateQueAndAnsList() {
-		fail("Not yet implemented");
+
+		qAObj.generateQueAndAnsList();
+		boolean emptyCellInArray = false;
+		
+		for (int i=0; i<qAObj.getQue_AnsList().length; i++) {
+			
+			if (qAObj.getQue_AnsList()[i] == 0) {
+				
+				emptyCellInArray = true;
+				break;
+			
+			}
+		
+		}
+		
+		boolean actualAnswer = !emptyCellInArray;
+		boolean expectedAnswer = true;
+		assertEquals(expectedAnswer, actualAnswer);
+	
 	}
 
 	@Test

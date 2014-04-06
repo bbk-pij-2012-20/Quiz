3d75abcbc0c748c87fdf4768713f59189af0b057
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-06 19:12:57 +0100

    getters for countryListIndex and questionListIndex. Javadoc.

--- Quiz/src/quiz/QueAndAns.java
+++ Quiz/src/quiz/QueAndAns.java
@@ -3,9 +3,18 @@ package quiz;
 public interface QueAndAns {
 
 	/**
+	 * Getter for questionListIndex
+	 */
+	int getQuestionListIndex();
+
+	/**
+	 * Getter for countryListIndex
+	 */
+	int getCountryListIndex();
+	
+	/**
 	 * Creates a list that encodes a single quiz question and 4 answers, one of
 	 * which is the correct answer.
-	 * 
 	 */
 	void generateQueAndAnsList();	
 		

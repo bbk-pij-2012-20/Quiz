b5f939ea4059c593e1991f272ff6e9f911872608
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-07 16:54:28 +0100

    getAnswer() corrected. Test passes.

--- Quiz/src/quiz/QuizDatabaseImpl.java
+++ Quiz/src/quiz/QuizDatabaseImpl.java
@@ -3,28 +3,102 @@ package quiz;
 public class QuizDatabaseImpl implements QuizDatabase {
 
 	// data acquired from Wikipedia, and for some air distances http://www.prokerala.com/travel
-	private String[] questionList = {"How many different colours are there in the flag of ","Roughly, how far (in km) is it from London to capital city of ","How high (in metres above sea level) is the highest point in "}; 	
+	private String[] questionList = {"How many different colours are there in the flag of ","Roughly, how far (in km) is it from London to capital city of ","How high (in metres above sea level) is the highest point in "};
 	private String[] countryList = {"Albania?","Azerbaijan?","Bulgaria?","Burkina Faso?","Djibouti?","Estonia?","Grenada?","Guatemala?","Kazakhstan?","Lesotho?"};
-	private int[][] answerList = {{2,2500,2800},{4,4500,4500},{3,2500,2900},{3,6000,750},{4,5900,2000},{3,1800,300},{3,7000,840},{5,8800,4200},{2,5600,7000},{4,9400,3500}}; 	
-	protected int questionListLength = questionList.length;
-	protected int countryListLength = countryList.length;
+	public int[][] answerList = {{2,2500,2800},{4,4500,4500},{3,2500,2900},{3,6000,750},{4,5900,2000},{3,1800,300},{3,7000,840},{5,8800,4200},{2,5600,7000},{4,9400,3500}}; 	
+	protected int questionListLength;
+	protected int countryListLength;
 
+	//explicit empty constructor.
+	public QuizDatabaseImpl() {
+		
+		questionListLength = questionList.length;
+		countryListLength = countryList.length;
+		
+	}
+	
+	public int getQuestionListLength() {
+		
+		return questionListLength;
+		
+	}
+	
+	public int getCountryListLength() {
+		
+		return countryListLength;
+		
+	}
+	
 	@Override
 	public String getQuestion(int questionListIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		System.out.printf("%s", checkArgumentInBounds(questionListIndex, questionList)?"" : "The questionList "); 	
+		
+		return questionList[questionListIndex];
+	
 	}
 
 	@Override
 	public String getCountry(int countryListIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		System.out.printf("%s", checkArgumentInBounds(countryListIndex, countryList)?"" : "The countryList "); 	
+		
+		return countryList[countryListIndex];
+	
 	}
 
 	@Override
 	public int getAnswer(int questionListIndex, int countryListIndex) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		try {
+			
+			if (questionListIndex >= answerList[0].length) {
+			
+				System.out.println("The questionListIndex passed is out of bounds of answerList");
+				throw new ArrayIndexOutOfBoundsException();
+			
+			} 
+			
+			if (countryListIndex >= answerList.length) {
+			
+				System.out.println("The countryListIndex passed is out of bounds of answerList");
+				throw new ArrayIndexOutOfBoundsException();
+			
+			}
+					
+		} catch (ArrayIndexOutOfBoundsException e) {
+		
+			e.printStackTrace();
+		
+		}
+		
+		return answerList[countryListIndex][questionListIndex];
+	
 	}
+	
+	private boolean checkArgumentInBounds(int listIndex, String[] list) {
+		
+		boolean outOfBounds = false;
+		
+		try {
+			
+			if (listIndex >= list.length) {
+			
+				System.out.println(" array is smaller than the index given. (Out of Bounds).");
+				outOfBounds = true;
+				throw new ArrayIndexOutOfBoundsException();
+			
+			}
+					
+		} catch (ArrayIndexOutOfBoundsException e) {
+		
+			e.printStackTrace();
+		
+		}
+
+		return !outOfBounds;
+		
+	}
+
 
 }

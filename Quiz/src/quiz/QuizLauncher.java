ccd5452e24559b1613d83ae26eb3c2465a7a3473
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-10 17:53:11 +0100

    added QuizLauncher which binds two servers: playerServer and quizServer to the same registry.

---
+++ Quiz/src/quiz/QuizLauncher.java
@@ -0,0 +1,50 @@
+package quiz;
+
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.registry.LocateRegistry;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+//import java.io.Serializable; 
+
+public class QuizLauncher {
+
+	public static void main(String[] args) {
+		
+		QuizLauncher quizLauncher = new QuizLauncher();
+		quizLauncher.launch();
+		
+	}
+	
+	public void launch(){
+		
+		if (System.getSecurityManager() == null) {
+			
+			System.setSecurityManager(new RMISecurityManager());
+			
+		}
+		
+		try {
+			
+			LocateRegistry.createRegistry(1099);
+			PlayerServer playerServer = new PlayerServer();
+			SetUpServer setUpServer = new SetUpServer();
+			String registryHost = "//localhost/";
+			String serviceName = "playerServer";
+			String serviceName2 = "setUpServer";
+			Naming.rebind(registryHost+serviceName,playerServer);
+			Naming.rebind(registryHost+serviceName2,setUpServer);
+
+		} catch (MalformedURLException e) {
+		
+			e.printStackTrace();
+						
+		} catch (RemoteException e) {
+		
+			e.printStackTrace();
+		
+		}
+	}
+}
+//java -Djava.security.policy=QuizSecurity.txt QuizLauncher

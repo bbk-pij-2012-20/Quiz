9ed52eb32674d1149eb52f1e673f29eb9ae6a891
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-10 18:22:19 +0100

    Added 4 new method called from PlayerClient getGameList(), getPlayerView(String), isGameOver(), updateView(String). Implementation code not written yet.

--- Quiz/src/quiz/PlayerServer.java
+++ Quiz/src/quiz/PlayerServer.java
@@ -2,18 +2,42 @@ package quiz;
 
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
-//import java.io.Serializable;
+import java.io.Serializable;
 
-public class PlayerServer extends UnicastRemoteObject implements PlayerService {
+public class PlayerServer extends UnicastRemoteObject implements PlayerService, Serializable {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 7708290464329235363L;
 
 	protected PlayerServer() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public char[] getPlayerView(char playerInput) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isGameOver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public char[] updateView(String readLine) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGameList() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }

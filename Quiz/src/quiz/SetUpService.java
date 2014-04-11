9f926b4d8d6ea03ddea5895ea2c26353833f74a2
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-11 12:18:32 +0100

    Added 4 method stubs isGameOver(), generateGameId(), updateView(int) and endGame(int)

--- Quiz/src/quiz/SetUpService.java
+++ Quiz/src/quiz/SetUpService.java
@@ -5,4 +5,34 @@ import java.rmi.Remote;
 
 public interface SetUpService extends Remote {
 
+
+	/**
+	 * Queries the status of the gameIsOver boolean in QuizControllerImpl. 
+	 * 
+	 * @return true if the game is over
+	 */
+	boolean isGameOver() throws RemoteException;
+
+	/**
+	 * generates a new game Id number 
+	 * 
+	 * @return
+	 * @throws RemoteException
+	 */
+	String generateGameId() throws RemoteException;
+
+	/**
+	 * 
+	 * @param setUpInput
+	 * @return
+	 */
+	char[] updateView(int setUpInput) throws RemoteException;
+
+	/**
+	 * 
+	 * @param setUpInput
+	 * @return
+	 */
+	int endGame(int setUpInput) throws RemoteException;
+
 }

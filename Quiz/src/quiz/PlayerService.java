f32771c8235e0f8321b3b88eb53a7dd3fe85afaa
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-11 22:17:16 +0100

    mostly re-written

--- Quiz/src/quiz/PlayerService.java
+++ Quiz/src/quiz/PlayerService.java
@@ -5,16 +5,16 @@ import java.rmi.RemoteException;
 import java.rmi.Remote;
 
 public interface PlayerService extends Remote, Serializable {
-
+	
 	/**
-	 * checks with QuizController on status of the current quiz game. 
-	 * While the game is not over, it expects an input to updateView(String)
-	 * 
-	 * 
-	 * @return
+	 * Called by the playerClient as the first activity in a quiz game for a player
+	 * to start playing a quiz.
+	 *  
+	 * @return a number as a String from the player, 1 of 3 game selections 
+	 * @throws RemoteException 
 	 */
-	boolean isGameOver();
-
+	String getGameList() throws RemoteException;
+	
 	/**
 	 * Takes an input from the player and passes it to QuizController 
 	 * which processes it and returns a String to be printed out to
@@ -23,16 +23,15 @@ public interface PlayerService extends Remote, Serializable {
 	 * @param readLine   the player's input to the UI which is only ever a single character
 	 * @return a String  which is returned to be output to UI
 	 */
-	char[] updateView(char input);
+	String updateView(char input) throws RemoteException;
 
 	/**
-	 * Called by the playerClient as the first activity in a quiz game for a player
-	 * to start playing a quiz.
-	 *  
-	 * @return a number as a String from the player, 1 of 3 game selections 
+	 * checks with QuizController on status of the current quiz game. 
+	 * While the game is not over, it expects an input to updateView(String)
+	 * 
+	 * 
+	 * @return
 	 */
-	String getGameList();
-
-
+	boolean isGameOver() throws RemoteException;
 	
 }

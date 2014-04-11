69e95ef1c646ada101f996e284406e4f9e176a53
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-11 10:56:11 +0100

    implementation of methods.

--- Quiz/src/quiz/PlayerServer.java
+++ Quiz/src/quiz/PlayerServer.java
@@ -4,35 +4,41 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 import java.io.Serializable;
 
+/** 
+ * PlayerServer generates a view of the UI for a player on server side. 
+ * It is called directly by the PlayerClient in order for the client to 
+ * see the UI view on the client side.
+ */
 public class PlayerServer extends UnicastRemoteObject implements PlayerService, Serializable {
 
+	QuizControllerImpl quizController = new QuizControllerImpl();
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7708290464329235363L;
 
-	protected PlayerServer() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	protected PlayerServer() throws RemoteException {}
 
 	@Override
 	public boolean isGameOver() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return quizController.getGameIsOverStatus(); 
+		
 	}
 
 	@Override
 	public char[] updateView(char input) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return quizController.updateView(input);
+	
 	}
 
 	@Override
 	public String getGameList() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "1. 2 questions\n3. 5 questions\n 3. 10 questions";
+		
 	}
-
 
 }

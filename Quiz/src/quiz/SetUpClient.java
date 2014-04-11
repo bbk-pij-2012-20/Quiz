77dfe46be5e801a9e2798130675cc2d13a8fc5b5
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-11 12:12:15 +0100

    setUpClient on client side. Eclipse-generated serialVersionUID.

--- Quiz/src/quiz/SetUpClient.java
+++ Quiz/src/quiz/SetUpClient.java
@@ -4,18 +4,39 @@ import java.rmi.Naming;
 import java.rmi.Remote;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
-//import java.io.Serializable;
+import java.io.Serializable;
 
-public class SetUpClient {
+public class SetUpClient implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2530077513828507665L;
+
 	public static void main(String[] args) {
 		
 		try {
-	
-			Remote remoteObj = Naming.lookup("//127.0.0.1:1099/Now");
-			SetUpService setUpService = (SetUpService) remoteObj;
-			System.out.println("hello I'm a setterUpperer");
-		
+			
+			Remote remoteObj2 = Naming.lookup("//127.0.0.1:1099/setup");
+			SetUpService setUpService = (SetUpService) remoteObj2;
+			System.out.println("Create a new quiz (y/n) ? " + setUpService.generateGameId());
+			
+			do {
+					System.out.println("Enter id number of game to terminate it");
+					int setUpInput = Integer.parseInt(System.console().readLine().trim());
+
+					if (setUpService.endGame(setUpInput) == 0) {
+						
+						System.out.println("No game by that Id, try again...");
+						
+					} else {
+
+						System.out.println(setUpService.updateView(setUpInput));
+					
+					}
+					
+			} while (!setUpService.isGameOver());		
+
 		} catch (RemoteException e) {
 		
 			e.printStackTrace();

c4a136569ce70f0eda1b24b5abcd22eb8e2aba53
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-04-15 12:21:21 +0100

    corrections to Javadoc

--- Quiz/src/quiz/QuizFactory.java
+++ Quiz/src/quiz/QuizFactory.java
@@ -1,35 +1,51 @@
 package quiz;
 
 import java.rmi.RemoteException;
+import java.util.Map;
 
 public interface QuizFactory {
 
 	/**
+	 * Generates three quizzes, one with 6 questions, one with 8 questions
+	 * and one with 10 questions. Calls generateAndStoreId() 
 	 * 
-	 * @return
+	 * @return HashMap              map of quizIds Integers and quizzes 
+	 *                              (an array with 6,8, or 10 questions, 
+	 *                              with 4 answers each).
 	 * @throws RemoteException
 	 */
-	QueAndAns[] getListOfQAndALists() throws RemoteException;
+	Map<Integer, QueAndAns[]> make3Quizzes() throws RemoteException;
 
 	/**
+	 * Getter for quiz
 	 * 
-	 * @param listOfQAndALists
+	 * @return quiz                 a list of queAndAns lists 
 	 * @throws RemoteException
 	 */
-	void setListOfQAndALists(QueAndAns[] listOfQAndALists) throws RemoteException;
+	QueAndAns[] getQuiz() throws RemoteException;
+
+	/**
+	 * Setter for quiz
+	 * 
+	 * @param quiz                 a list of queAndAns lists 
+	 * @throws RemoteException
+	 */
+	void setQuiz(QueAndAns[] quiz) throws RemoteException;
 	
 	/**
+	 * Getter for constant value noOfAnswersPerQuestion.
 	 * 
-	 * @return
+	 * @return noOfAnswersPerQuestion   answers per question int (always 4)
 	 * @throws RemoteException
 	 */
 	int getNoOfAnswersPerQuestion() throws RemoteException;
 
 	/**
+	 * Getter for noOfQuestionsPerQuiz 
 	 * 
-	 * @return
+	 * @return noOfQuestionsPerQuiz     questions per quiz int (6,8 or 10)
 	 * @throws RemoteException
 	 */
-	int getTotalNoOfQuestions() throws RemoteException;
+	int getNoOfQuestionsPerQuiz() throws RemoteException;
 
 }
